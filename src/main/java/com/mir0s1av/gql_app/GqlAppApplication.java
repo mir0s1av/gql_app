bae908ac7b054819887e973bdb752526618f52fe
mir0s1av <miroslav.babenko@icloud.com>
2025-02-05 00:18:09 +0100

    Initial commit

---
+++ src/main/java/com/mir0s1av/gql_app/GqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.mir0s1av.gql_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mir0s1av/gql_app/GqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mir0s1av.gql_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
